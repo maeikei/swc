f641b2844c1a7e7495014fdb016fce29d46f62af
ma yingkui <maeikei@gmail.com>
2015-11-26 23:50:32 +0900

    Update login.js

--- www/swc/public/assets/js/login.js
+++ www/swc/public/assets/js/login.js
@@ -39,7 +39,7 @@ $(function(){
       name: "RSASSA-PKCS1-v1_5",
       modulusLength: 4096, //can be 1024, 2048, or 4096
       publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-      hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+      hash: {name: "SHA-512"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
       },
       true, //whether the key is extractable (i.e. can be used in exportKey)
       ["sign", "verify"] //can be any combination of "sign" and "verify"
@@ -62,7 +62,7 @@ $(function(){
       privateKey,
       {   //these are the algorithm options
       name: "RSASSA-PKCS1-v1_5",
-      hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+      hash: {name: "SHA-512"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
       },
       false, //whether the key is extractable (i.e. can be used in exportKey)
       ["sign"] //"sign" for private key imports
