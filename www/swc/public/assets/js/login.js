1d32f0fbd96ebd8383b0932ff5990441d43e776e
ma yingkui <maeikei@gmail.com>
2015-11-27 00:36:53 +0900

    Update login.js

--- www/swc/public/assets/js/login.js
+++ www/swc/public/assets/js/login.js
@@ -1,13 +1,15 @@
 $(function(){
+  function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+  }
   function str2ab(str) {
     var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
     var bufView = new Uint16Array(buf);
-    for (var i=0, strLen=str.length; i++) {
+    for (var i=0, strLen=str.length; i<strLen; i++) {
       bufView[i] = str.charCodeAt(i);
     }
     return buf;
   }
-
   function sendSignToken(privateKey) {
     var token = localStorage.getItem('swc.login.token');
     var abToken = str2ab(token); 
