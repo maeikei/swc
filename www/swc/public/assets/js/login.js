8d0311def95be278db83dcce7f273138c336287c
ma yingkui <maeikei@gmail.com>
2015-11-26 23:29:54 +0900

    Update login.js

--- www/swc/public/assets/js/login.js
+++ www/swc/public/assets/js/login.js
@@ -35,28 +35,26 @@ $(function(){
       });
   }
   function createKeyPair() {
-      window.crypto.subtle.generateKey(
-        {
-          name: "RSASSA-PKCS1-v1_5",
-          modulusLength: 4096, //can be 1024, 2048, or 4096
-          publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-          hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-          },
-          true, //whether the key is extractable (i.e. can be used in exportKey)
-          ["sign", "verify"] //can be any combination of "sign" and "verify"
-          )
-          .then(function(key){
-            //returns a keypair object
-            //console.log(key);
-            console.log(key.publicKey);
-            console.log(key.privateKey);
-            saveKey(key.privateKey,'privateKey');
-            saveKey(key.publicKey,'publicKey',true);
-        })
-        .catch(function(err){
-            console.error(err);
-        });
-    }
+    window.crypto.subtle.generateKey({
+      name: "RSASSA-PKCS1-v1_5",
+      modulusLength: 4096, //can be 1024, 2048, or 4096
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+      },
+      true, //whether the key is extractable (i.e. can be used in exportKey)
+      ["sign", "verify"] //can be any combination of "sign" and "verify"
+    )
+    .then(function(key){
+      //returns a keypair object
+      //console.log(key);
+      console.log(key.publicKey);
+      console.log(key.privateKey);
+      saveKey(key.privateKey,'privateKey');
+      saveKey(key.publicKey,'publicKey',true);
+    })
+    .catch(function(err){
+      console.error(err);
+    });
   }
   function importKey(privateKey) {
     window.crypto.subtle.importKey(
@@ -68,14 +66,14 @@ $(function(){
       },
       false, //whether the key is extractable (i.e. can be used in exportKey)
       ["sign"] //"sign" for private key imports
-      )
-      .then(function(privateKey){
-        //returns a publicKey (or privateKey if you are importing a private key)
-        console.log(privateKey);
-      })
-    	.catch(function(err){
-    	  console.error(err);
-    	});
+    )
+    .then(function(privateKey){
+      //returns a publicKey (or privateKey if you are importing a private key)
+      console.log(privateKey);
+    })
+    .catch(function(err){
+      console.error(err);
+    });
   }
   var privateKey = localStorage.getItem('swc.login.privateKey');
   //console.log(privateKey);
