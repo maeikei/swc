5395bebffc60e5dcd9435c8c4e13c1c2ba86f38c
ma yingkui <maeikei@gmail.com>
2015-11-27 00:34:24 +0900

    Update login.js

--- www/swc/public/assets/js/login.js
+++ www/swc/public/assets/js/login.js
@@ -2,7 +2,7 @@ $(function(){
   function str2ab(str) {
     var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
     var bufView = new Uint16Array(buf);
-    for (var i=0, strLen=str.length; i &lt; strLen; i++) {
+    for (var i=0, strLen=str.length; i++) {
       bufView[i] = str.charCodeAt(i);
     }
     return buf;
