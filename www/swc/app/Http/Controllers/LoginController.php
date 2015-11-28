b40cebc82a4259984729319cbda11d9d80700355
ma yingkui <maeikei@gmail.com>
2015-11-28 10:44:35 +0900

    Update LoginController.php

--- www/swc/app/Http/Controllers/LoginController.php
+++ www/swc/app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ use Log;
 
 class LoginController extends Controller
 {
-    const KEYROOT = storage_path() + '/publicKeys/';
+     $keyRoot_ = '';
     /**
      * Display a listing of the resource.
      *
@@ -41,6 +41,7 @@ class LoginController extends Controller
      */
     public function store(Request $request)
     {
+        self::$keyRoot_ = storage_path() + '/publicKeys/';
         //
         //Log::info('store: '.$request->method());
         if ($request->isMethod('post')) {
@@ -53,7 +54,7 @@ class LoginController extends Controller
           if (isset($bodyJson->publicKey)) {
             $token = hash('sha512',$bodyJson->publicKey);
             Log::info($token);
-            $keyPath = self::KEYROOT + $token +'/';
+            $keyPath = self::$keyRoot_ + $token +'/';
             $output = shell_exec('mkdir -p' + $keyPath);
             Log::info($output);
             file_put_contents($keyPath+'/publicKey.pem', $bodyJson->publicKey);
@@ -64,7 +65,7 @@ class LoginController extends Controller
             Log::info($token);
             $signature = $bodyJson->signature;
             Log::info($signature);
-            $keyPath = self::KEYROOT + $token +'/publicKey.pem';
+            $keyPath = self::$keyRoot_ + $token +'/publicKey.pem';
             $pubkeyid = openssl_pkey_get_public($keyPath);
             $ok = openssl_verify($token, $signature, $pubkeyid);
             openssl_free_key($pubkeyid);
