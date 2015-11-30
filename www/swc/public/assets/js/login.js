385cbda94fa4b66a7dbb3b5d3ff186c3a6f32780
ma yingkui <maeikei@gmail.com>
2015-11-30 23:35:17 +0900

    Update login.js

--- www/swc/public/assets/js/login.js
+++ www/swc/public/assets/js/login.js
@@ -129,7 +129,7 @@ swc.rsa.savePublicKey = function(key) {
 }
 swc.rsa.savePrivateKey = function(key) {
   window.crypto.subtle.exportKey(
-    "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+    "pkcs8", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
     key //can be a publicKey or privateKey, as long as extractable was true
     )
     .then(function(keydata){
@@ -169,13 +169,13 @@ swc.rsa.createKeyPair = function () {
 }
 swc.rsa.importKey = function (privateKey) {
   window.crypto.subtle.importKey(
-    "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+    "pkcs8", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
     privateKey,
     {   //these are the algorithm options
     name: "RSASSA-PKCS1-v1_5",
     hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
     },
-    false, //whether the key is extractable (i.e. can be used in exportKey)
+    true, //whether the key is extractable (i.e. can be used in exportKey)
     ["sign"] //"sign" for private key imports
   )
   .then(function(privateKey){
