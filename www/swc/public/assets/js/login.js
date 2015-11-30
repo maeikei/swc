5eea807d19ff6a8ab4929f95b3ded9759d0b9f86
ma yingkui <maeikei@gmail.com>
2015-11-30 23:27:09 +0900

    Update login.js

--- www/swc/public/assets/js/login.js
+++ www/swc/public/assets/js/login.js
@@ -145,9 +145,9 @@ swc.rsa.savePrivateKey = function(key) {
 swc.rsa.createKeyPair = function () {
   window.crypto.subtle.generateKey({
     name: "RSASSA-PKCS1-v1_5",
-    modulusLength: 4096, //can be 1024, 2048, or 4096
+    modulusLength: 2048, //can be 1024, 2048, or 4096
     publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-    hash: {name: "SHA-512"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+    hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
     },
     true, //whether the key is extractable (i.e. can be used in exportKey)
     ["sign", "verify"] //can be any combination of "sign" and "verify"
@@ -171,7 +171,7 @@ swc.rsa.importKey = function (privateKey) {
     privateKey,
     {   //these are the algorithm options
     name: "RSASSA-PKCS1-v1_5",
-    hash: {name: "SHA-512"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+    hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
     },
     false, //whether the key is extractable (i.e. can be used in exportKey)
     ["sign"] //"sign" for private key imports
