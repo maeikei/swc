8d15e72d870a93c4b937ec636c39a79b837f680c
ma yingkui <maeikei@gmail.com>
2015-12-03 00:41:46 +0900

    Update WelcomeController.php

--- www/swc/app/Http/Controllers/WelcomeController.php
+++ www/swc/app/Http/Controllers/WelcomeController.php
@@ -20,6 +20,10 @@ class WelcomeController extends Controller
         //
         $files = shell_exec('find /media/hdd/ssroot/livestreaming/wv.ss.*.mov -type f -print0 | xargs -0 ls -t | sed -e "s/\/media\/hdd\/ssroot\/livestreaming\///"');
         Log::info('$files=<' . $files . '>');
+        $FilesArray = explode('\n',$files);
+        foreach ($FilesArray as $file) {
+            Log::info('$file=<' . $file . '>');
+        }
         return view('welcome');
     }
 
