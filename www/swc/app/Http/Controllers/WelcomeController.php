961fc2751c31ab32032e06da5ab5f439cbc77673
ma yingkui <maeikei@gmail.com>
2015-12-03 01:41:30 +0900

    Update WelcomeController.php

--- www/swc/app/Http/Controllers/WelcomeController.php
+++ www/swc/app/Http/Controllers/WelcomeController.php
@@ -20,20 +20,16 @@ class WelcomeController extends Controller
         //
         $files = shell_exec('find /media/hdd/ssroot/livestreaming/wv.ss.*.mov -type f -print0 | xargs -0 ls -t | sed -e "s/\/media\/hdd\/ssroot\/livestreaming\///"');
         //Log::info('$files=<' . $files . '>');
-        $FilesArray = explode("\n",$files);
+        $FilesArray = explode(".mov",$files);
         $FileName = [];
         foreach ($FilesArray as $file) {
-            $name = str_replace("wv.ss.",'',$file);
+            $name = str_replace("\nwv.ss.",'',$file);
             $name = str_replace(".mov",'',$name);
             //Log::info('$file=<' . $file . '>');
             //Log::info('$name=<' . $name . '>');
             array_push($FileName,$name);
         }
-        $FileData = array(
-            'name'  => $FileName,
-            'src'   => $files
-            );
-        return view('welcome',['src'=>$files,'name' =>$FileName]);
+        return view('welcome',['name' =>$FileName]);
     }
 
     /**
