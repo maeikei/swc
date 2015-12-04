e930aed6655b231ab1f4cf2298d316b3dafba142
ma yingkui <maeikei@gmail.com>
2015-12-04 22:15:35 +0900

    Update master.blade.php

--- www/swc/resources/views/layouts/master.blade.php
+++ www/swc/resources/views/layouts/master.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>Security Wifi Camera</title>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <!-- Optional theme -->
