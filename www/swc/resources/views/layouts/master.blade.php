9a2470083a1143eb39b9191b4dd8c47b96d5ba63
ma yingkui <maeikei@gmail.com>
2015-12-04 23:43:02 +0900

    Update master.blade.php

--- www/swc/resources/views/layouts/master.blade.php
+++ www/swc/resources/views/layouts/master.blade.php
@@ -7,18 +7,48 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     </head>
     <body>
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <div class="collapse navbar-collapse">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Top</a></li>
-                        </ul>
-                        <p class="navbar-text navbar-right">ようこそ <a href="#" class="navbar-link">ゲスト</a> さん。</p>
-                    </div>
-                </div>
-            </div>
-        </nav>
+ <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/about">{{trans('navbar.watorvapor_about')}}</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="/xuniverse">{{trans('navbar.xuniverse')}}</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+ 			<li>
+			  <button type="button" class="btn btn-default navbar-btn language_button" value="en" >
+			    <img src="{{ asset('/flags/us.svg') }}" alt="button" />
+			  </button>
+			</li>
+ 			<li>
+			  <button type="button" class="btn btn-default navbar-btn language_button" value="zh" >
+			    <img src="{{ asset('/flags/cn.svg') }}" alt="button" />
+			  </button>
+			</li>
+ 			<li>
+			  <button type="button" class="btn btn-default navbar-btn language_button" value="ja" >
+			    <img src="{{ asset('/flags/jp.svg') }}" alt="button" />
+			  </button>
+			</li>
+ 			<li>
+			  <button type="button" class="btn btn-default navbar-btn language_button" value="ru" >
+			    <img src="{{ asset('/flags/ru.svg') }}" alt="button" />
+			  </button>
+			</li>
+			<li class="active"><a href="/">Fixed top <span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
         @section('sidebar')
             This is the master sidebar.
         @show
