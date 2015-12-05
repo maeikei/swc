5ba5a4bf5965a4b42355ac1ddc68004cf0611744
ma yingkui <maeikei@gmail.com>
2015-12-05 12:38:28 +0900

    Update master.blade.php

--- www/swc/resources/views/layouts/master.blade.php
+++ www/swc/resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <!-- Optional theme -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="{{{asset('/assets/css/master.css')}}}">
     </head>
     <body>
  <!-- Fixed navbar -->
