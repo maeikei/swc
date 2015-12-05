34a003581e19350e7d2cc02a861fc09f5f43540b
ma yingkui <maeikei@gmail.com>
2015-12-05 19:10:15 +0900

    Update welcome.blade.php

--- www/swc/resources/views/welcome.blade.php
+++ www/swc/resources/views/welcome.blade.php
@@ -7,9 +7,7 @@
             <div class="clipGroupMonth">
             @foreach ($ClipsMonth as $day => $ClipsDay)
                 <div class="clipGroupDay">
-                <br/>
                 <a href="/swc/play/{{{$year}}}.{{{$month}}}.{{{$day}}}">{{{$year}}}.{{{$month}}}.{{{$day}}}</a>
-                <br/>
                 @foreach ($ClipsDay as $Clip)
                     <a href="/swc/play/{{{$Clip}}}">{{{$Clip}}}</a>
                 @endforeach
