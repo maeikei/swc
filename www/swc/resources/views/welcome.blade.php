3c3c09a2768ab3c73c409662392a72411935144a
ma yingkui <maeikei@gmail.com>
2015-12-05 19:06:27 +0900

    Update welcome.blade.php

--- www/swc/resources/views/welcome.blade.php
+++ www/swc/resources/views/welcome.blade.php
@@ -2,16 +2,22 @@
 
 @section('content')
     @foreach ($clipsSortTime as $year => $ClipsYear)
+        <div class="clipGroupYear">
         @foreach ($ClipsYear as $month => $ClipsMonth)
+            <div class="clipGroupMonth">
             @foreach ($ClipsMonth as $day => $ClipsDay)
+                <div class="clipGroupDay">
                 <br/>
                 <a href="/swc/play/{{{$year}}}.{{{$month}}}.{{{$day}}}">{{{$year}}}.{{{$month}}}.{{{$day}}}</a>
                 <br/>
                 @foreach ($ClipsDay as $Clip)
                     <a href="/swc/play/{{{$Clip}}}">{{{$Clip}}}</a>
                 @endforeach
+                </div>
             @endforeach
+            </div>
         @endforeach
+        </div>
     @endforeach
 @endsection
 
