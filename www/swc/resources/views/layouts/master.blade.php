c351577f255123bf07ec06c7c8fd56c4ef9a2a05
ma yingkui <maeikei@gmail.com>
2015-12-05 19:28:41 +0900

    Update master.blade.php

--- www/swc/resources/views/layouts/master.blade.php
+++ www/swc/resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/about">about</a>
+          <a class="navbar-brand" href="/about">About</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
