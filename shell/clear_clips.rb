58a69124c55f2f60be3342c2fac2a317f22608df
ma yingkui <maeikei@gmail.com>
2016-06-17 07:07:34 +0900

    Update clear_clips.rb

--- shell/clear_clips.rb
+++ shell/clear_clips.rb
@@ -3,5 +3,10 @@ files_last_one=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 600`
 files_last_two=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 400`
 lastOne = files_last_one.split(("\n"));
 lastTwo = files_last_two.split(("\n"));
-p lastOne
-p lastTwo
+p lastOne.length
+p lastTwo.length
+if(lastOne.length != lastTwo.length) then
+  lastTwo.each do |mov|
+    p mov
+  end
+end
