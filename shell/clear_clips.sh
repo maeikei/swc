5eff33f08c274cccc635c941e3058797b2b94f95
ma yingkui <maeikei@gmail.com>
2016-06-17 06:59:09 +0900

    Update clear_clips.sh

--- shell/clear_clips.sh
+++ shell/clear_clips.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-files=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 600`
-echo ${files}
+files_last600=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 600`
+echo ${files_last600}
+files_last400=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 400`
+echo ${files_last400}
 
