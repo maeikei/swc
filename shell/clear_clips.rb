c802d42288c2fd257669c6fc0b9a4c091ad5ed0a
ma yingkui <maeikei@gmail.com>
2016-06-17 07:04:23 +0900

    Update clear_clips.rb

--- shell/clear_clips.rb
+++ shell/clear_clips.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
-files_last600=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 600`
-files_last400=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 400`
-files_last600.each_line {|file|
-  p file
-}
+files_last_one=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 600`
+files_last_two=`ls -r /media/hdd/ssroot/livestreaming/wv.ss.*.mov | tail -n 400`
+lastOne = files_last_one.split(("\n"));
+lastTwo = files_last_two.split(("\n"));
+p lastOne
+p lastTwo
